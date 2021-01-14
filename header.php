65683f12a4e1086f186657e34fbac6347fa9b849
davy1ex <dezy1ex@gmail.com>
2021-01-14 09:40:45 +0400

    redesign

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div class="banner" style="background-image: url('img/banner.jpg');">
+    <div class="banner" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/assets/img/banner.jpg' ?>)" >
         <header>
             <div class="wrapper-header">
                 <div class="logo"><? bloginfo( 'name' ) ?></div>
--- index.php
+++ index.php
@@ -1,35 +1,35 @@
 <? get_header(); ?>
-<div class="main-content">
-    <div class="container">
-        <div class="desc">
-            <div class="date">
-                <div class="days">21.</div>
-                <div class="mounth">12.</div>
-                <div class="year">21</div>
-            </div>
-    
-            <div class="event-text">
-                <div class="large">LARGEST GAME EVENT</div>
-                <div class="medium">SAMARA UNIVERSITY, CORP. 42, CAB. 4</div>
-            </div>
 
-            <div class="social">
-                <div class="social_item"><a href=""><img src="img/vk.svg" alt=""></a></div>
-                <div class="social_item"><a href=""><img src="img/inst.png" alt=""></a></div>
-                <div class="social_item"><a href=""><img src="img/twitter.png" alt=""></a></div>
-            </div>
-        </div> 
-    </div> 
-    
+    <!-- <div class="banner" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/assets/img/banner.jpg' ?>)"> -->
+        <!-- <div class="container"> -->
+            <div class="desc">
+                <div class="date">
+                    <div class="days">21.</div>
+                    <div class="mounth">12.</div>
+                    <div class="year">21</div>
+                </div>
+        
+                <div class="event-text">
+                    <div class="large">LARGEST GAME EVENT</div>
+                    <div class="medium">SAMARA UNIVERSITY, CORP. 42, CAB. 4</div>
+                </div>
 
+                <div class="social">
+                    <div class="social_item"><a href=""><img src="img/vk.svg" alt=""></a></div>
+                    <div class="social_item"><a href=""><img src="img/inst.png" alt=""></a></div>
+                    <div class="social_item"><a href=""><img src="img/twitter.png" alt=""></a></div>
+                </div>
+            </div> 
+        <!-- </div>  -->
+    </div>
+    
+    
+    <div class="main-content">
     <div class="events-container container">
         <div class="title">
             EVENTS
         </div>
         <?php
-            // if ( have_posts() ):
-            //     while( have_posts() ): 
-            //         the_post( );
             $events = get_posts( array(
                 'category_name' => 'events'
             ) );
@@ -39,13 +39,11 @@
                 ?>
         
         <a class="event" href="<?php the_permalink() ?>">
-            <!-- <div class="event"> -->
             <img class="event-img" src="<?php the_post_thumbnail_url() ?>" alt="">
 
             <div class="event-desc">
                 <?php echo get_the_excerpt() ?>
             </div>
-            <!-- </div> -->
         </a>
             
             
@@ -56,9 +54,7 @@
         ?>
             
                     
-        <?php 
-                // endwhile;
-                    
+        <?php                     
             else: ?>
                 <?php get_template_part( 'tmp/no-posts' ); ?>
             <? endif; ?>
