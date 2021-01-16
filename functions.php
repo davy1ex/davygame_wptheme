4862bbaf82775f0efbd32a25b3156e6d6506cb17
davy1ex <dezy1ex@gmail.com>
2021-01-17 00:31:40 +0400

    added taxowtf?

--- functions.php
+++ functions.php
@@ -14,33 +14,33 @@ function davygame_scripts() {
     wp_enqueue_style( 'style.css', get_stylesheet_directory_uri() . '/assets/css/style.css');
 }
 
-function davygame_customize_register($wp_customize) {
-    // new tab in customize
-    $wp_customize->add_section('footer', array(
-        'title' => 'footer',
-        'priority' => 1,
-    ));
+// function davygame_customize_register($wp_customize) {
+//     // new tab in customize
+//     $wp_customize->add_section('footer', array(
+//         'title' => 'footer',
+//         'priority' => 1,
+//     ));
 
-    // settings field
-    $wp_customize->add_setting('footer_text', array(
-        'default' => 'example text',
-        'sanitize_callback' => 'sanitize_textarea_field',
-        'transport' => 'postMessage'
-    ));
+//     // settings field
+//     $wp_customize->add_setting('footer_text', array(
+//         'default' => 'example text',
+//         'sanitize_callback' => 'sanitize_textarea_field',
+//         'transport' => 'postMessage'
+//     ));
 
-    $wp_customize->add_control($setting_name, array(
-        'section' => 'footer',
-        'type' => 'textarea',
-        'label' => 'Текст в подвале',
-    ));
+//     $wp_customize->add_control($setting_name, array(
+//         'section' => 'footer',
+//         'type' => 'textarea',
+//         'label' => 'Текст в подвале',
+//     ));
 
-    $wp_customize->selective_refresh->add_partial($setting_name, array(
-        'selector' => '.site-info', //должен содержать class или id элемента с текстом в подвале
-        'render_callback' => function() use ($setting_name) {
-            return nl2br(esc_html(get_theme_mod($setting_name)));
-        }
-    ));
-}
+//     $wp_customize->selective_refresh->add_partial($setting_name, array(
+//         'selector' => '.site-info', //должен содержать class или id элемента с текстом в подвале
+//         'render_callback' => function() use ($setting_name) {
+//             return nl2br(esc_html(get_theme_mod($setting_name)));
+//         }
+//     ));
+// }
 
 function davygame_register_types() {
     register_post_type('events', [
@@ -88,4 +88,23 @@ function davygame_register_types() {
 		'hierarchical'       => false,
 		'supports'           => array('title')//,'editor','author','thumbnail','excerpt','comments')
     ]);
+
+    register_taxonomy( 'time_start', [ 'events' ], [ 
+		'label'                 => '', // определяется параметром $labels->name
+		'labels'                => [
+			'name'              => 'start time',
+			'singular_name'     => 'start time',
+			'search_items'      => 'Search start time',
+			'all_items'         => 'All start times',
+			'view_item '        => 'View start time',
+			'edit_item'         => 'Edit start time',
+			'update_item'       => 'Update start time',
+			'add_new_item'      => 'Add New start time',
+			'new_item_name'     => 'New start time Name',
+			'menu_name'         => 'all start time',
+		],
+		'description'           => '', // описание таксономии
+		'public'                => true,		
+		'hierarchical'          => true,
+	] );
 }
